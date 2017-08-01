e6c92f71c849d94c015003ef841277f36b635021
Gary DiCiano <garydiciano4@gmail.com>
2017-07-31 21:14:04 -0400

    Player physics pretty much nailed down

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,33 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
+	//Used for the Flip() function
+	public bool facingRight = true;
 
-	private Rigidbody2D rb;
+	//Tells us when the player is airborne 
+	public bool jump;
 
-	//Used for GetAxis
-	public float h;
-	//Fastest the player can move
+	//So we can decide if a player can double jump
+	public bool doubleJump;
+
+	//i.e. Speed
 	public float moveForce;
 	public float maxSpeed;
 
-	//Let's us know when the player is airborne or on the ground
-	public bool jump, grounded;
-	public Transform groundCheck;
-	//Helps determine jump height
+	//Power of a jump
 	public float jumpForce;
 
-	// Use this for initialization
-	void Awake () {
+	//Rigidbody2D reference
+	private Rigidbody2D rb;
+
+	//Reference to the bullet prefab and where to spawn it
+	public GameObject bullet;
+	public GameObject bulletSpawn;
+
+	//How fast a bullet can be fired
+	public float fireRate;
+	private float nextFire = 0.25f;
+
+	//To check when the player is touching the ground
+	public Transform groundCheck;
+	public bool grounded;
+
+	//Our GetAxis value for movement
+	public float h;
+
+	void Awake()
+	{
 		rb = GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Uses a 2D linecast to see if the player is touching the ground
+
+	void Update()
+	{	
+		//Uses a vertical linecast to see if the player is touching the ground
 		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 
-		//If the jump button was pressed while the player is grounded
 		if (Input.GetButtonDown ("Jump") && grounded) 
 		{
 			grounded = false;
@@ -36,16 +55,34 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	void FixedUpdate () {
-		//Movement
+	void FixedUpdate()
+	{
 		h = Input.GetAxis ("Horizontal");
 
+		//The following two conditionals create a speed cap
 		if (h * rb.velocity.x < maxSpeed) 
 		{
 			rb.AddForce (Vector2.right * h * moveForce);
 		}
 
-		//Makes the player jump
+		if (Mathf.Abs (rb.velocity.x) > maxSpeed) 
+		{
+			//Mathf.Sign returns -1 or 1 depending on the sign of the input
+			rb.velocity = new Vector2 (Mathf.Sign (rb.velocity.x) * maxSpeed, rb.velocity.y);
+		}
+
+		//Flips when hitting 'right' and facing left
+		if (h > 0 && !facingRight)
+			Flip ();
+		//Flips when hitting 'left' and facing right
+		else if (h < 0 && facingRight)
+			Flip ();
+
+		if(grounded)
+		{
+			doubleJump = false;
+		}
+
 		if (jump) 
 		{
 			//rb.AddForce(new Vector2(0f, jumpForce));
@@ -53,5 +90,21 @@ public class PlayerController : MonoBehaviour {
 			jump = false;
 			//Debug.Log ("Jumped");
 		}
+
+		//Double Jump
+		if (Input.GetButtonDown("Jump") && !grounded && !doubleJump) 
+		{
+			rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+			doubleJump = true;
+		}
 	}
-}
+
+	//Changes rotation of the player
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+} 
