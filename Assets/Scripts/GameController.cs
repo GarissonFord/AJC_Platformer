8ff4130bdaac5d1c35ab00f54c5a8063a84b5454
Gary DiCiano <garydiciano4@gmail.com>
2017-08-14 20:03:58 -0400

    Added score counter and gradual speed increase
    The math that dictates the speed increase is wonky and needs to be
    readjusted, ideally I want the speed of a platform to be 10 + (1 * x)
    where x increments every 10 seconds

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour 
+{
+	public Text scoreText;
+
+	void Update()
+	{
+		scoreText.text = "Score: " + Time.timeSinceLevelLoad.ToString();
+	}
+}
--- Assets/Scripts/PlatformMove.cs
+++ Assets/Scripts/PlatformMove.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour {
 
-	public float moveSpeed;
+	public float moveSpeed = 10.0f;
+	public float timeSpeedMultiplier;
 	public Rigidbody2D rb;
 
 	void Awake()
 	{
+		//This should adjust the speed per each 10 seconds
+		timeSpeedMultiplier = Time.timeSinceLevelLoad % 10.0f;
+		moveSpeed += timeSpeedMultiplier;
+		//Let's test this out...
 		rb = GetComponent<Rigidbody2D> ();
 		rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
 	}
