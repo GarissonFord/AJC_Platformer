22dbd729286dd9be279554931c3df15d651ba832
Gary DiCiano <garydiciano4@gmail.com>
2017-08-21 21:15:45 -0400

    More detailed game over screen

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -7,16 +7,19 @@ using UnityEngine.UI;
 public class DestroyByContact : MonoBehaviour 
 {
 	public Text gameOverText;
+	public GameObject gameOverImage;
 
 	void Start()
 	{
 		gameOverText.text = "";
+		gameOverImage.SetActive (false);
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.CompareTag("Player"))
 		{
+			gameOverImage.SetActive (true);
 			gameOverText.text = "Game Over, you fuck";
 			Invoke("RestartGame", 3.0f);
 		}
